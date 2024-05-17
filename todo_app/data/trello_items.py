0c5355014a1c3c64ab953a9a3b2daddc24566b6e
mike.collingwood <mike.collingwood@digital.cabinet-office.gov.uk>
2024-05-17 15:56:39 +0100

    Add update state button

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -4,13 +4,9 @@ from flask import session
 import requests
 
 # use env var for prefix
-cardsUrl = "https://api.trello.com/1/cards"
-boardUrl = "https://api.trello.com/1/boards/65c4fed365273e251af75aa5"
-listIds = {
-    "todo": "65c4fed42c4a7016163343f9",
-    "doing": "65c4fed365273e251af75aa5",
-    "done": "65c4fed475af8113b502c830"
-}
+cards_url = "https://api.trello.com/1/cards"
+board_url = "https://api.trello.com/1/boards/65c4fed365273e251af75aa5"
+todo_list_id = "65c4fed42c4a7016163343f9"
 
 headers = {
     "Accept": "application/json"
@@ -20,16 +16,16 @@ query = {
     'token': getenv('TRELLO_API_TOKEN')
 }
 
-def get_items():
+def get_list_organised_items():
     """
-    Fetches all saved items from the trello board.
+    Fetches all saved items for each status in the trello board.
 
     Returns:
-        list: The list of items.
+        list: The list of list names with corresponding items.
     """
     items_response = requests.request(
         "GET",
-        f"{boardUrl}/cards",
+        f"{board_url}/cards",
         headers=headers,
         params=query
     )
@@ -37,18 +33,57 @@ def get_items():
 
     lists_response = requests.request(
         "GET",
-        f"{boardUrl}/lists",
+        f"{board_url}/lists",
         headers=headers,
         params=query
     )
-    lists = json.loads(lists_response.text)
-    list_names = { list['id']: list['name'] for list in lists }
+    list_organised_items = [{
+        "name": list["name"],
+        "entries": [{
+            "id": item["id"],
+            "name": item["name"],
+            } for item in items if item["idList"] == list["id"]]
+        } for list in json.loads(lists_response.text) ]
 
-    return [{
-        "id": item['id'],
-        "status": list_names[item['idList']],
-        "title": item['name'],
-    } for item in items]
+    return list_organised_items
+
+def get_lists():
+    """
+    Fetch all lists in board
+
+    Returns:
+        list: list of lists in the board
+    """
+    lists_response = requests.request(
+        "GET",
+        f"{board_url}/lists",
+        headers=headers,
+        params=query
+    )
+    
+    return json.loads(lists_response.text)
+
+def update_item_status(item_id, list_id):
+    """
+    
+    """
+    update_item_status_query = {}
+    update_item_status_query.update(query)
+    update_item_status_query.update({
+        "idList": list_id
+        })
+
+    response = requests.request(
+        "PUT",
+        f'{cards_url}/{item_id}',
+        headers=headers,
+        params=update_item_status_query
+    )
+
+    print("response")
+    print(response.content)
+
+    return response
 
 def add_item(title):
     """
@@ -64,14 +99,14 @@ def add_item(title):
     add_item_query.update(query)
     add_item_query.update({
         "name": title,
-        "idList": listIds["todo"]
+        "idList": todo_list_id
         })
 
-    items_response = requests.request(
+    response = requests.request(
         "POST",
-        cardsUrl,
+        cards_url,
         headers=headers,
         params=add_item_query
     )
 
-    return items_response
+    return response
